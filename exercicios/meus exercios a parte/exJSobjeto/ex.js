666a7222c51c6cd23d45db77e25975855d575cb1
LuUxBrr <129239543+LuUxBrr@users.noreply.github.com>
2023-03-29 21:23:45 -0300

    Initial commit

---
+++ exercicios/meus exercios a parte/exJSobjeto/ex.js
@@ -0,0 +1,8 @@
+var carro = {
+    motor: 'V8',
+    modelo:'normal',
+    ano: '2012'
+}
+window.alert(`Ano do carro: ${carro.ano}`)
+window.alert(`motor: ${carro.motor}`)
+window.alert(`modelo: ${carro['modelo']}`)
